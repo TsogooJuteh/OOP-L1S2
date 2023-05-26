41bdd87d09ef9f6aababa875db57508a05ed2eae
TsogooJuteh <ttsogtoi@gmail.com>
2023-05-27 01:07:49 +0300

    27/5/2023

--- zadacha6 copy.cpp
+++ zadacha6 copy.cpp
@@ -109,7 +109,7 @@ public:
         info = data;
     }
     Node(const T& info, Node<T>* prev, Node<T>* next)
-        : next(next), prev(prev),  info(info) {}
+        : info(info), prev(prev), next(next) {}
     T getInfo() { return info; }
     void setInfo(T value) { info = value; }
     Node<T>* getNext() { return next; }
@@ -132,14 +132,162 @@ public:
         head = tail = nullptr;
         count = 0;
     }
-    virtual void push_back(T value) = 0;
-    virtual void push_front(T value) = 0;
-    virtual void pop_front() = 0;
-    virtual void pop_back() = 0;
+    virtual void push(T value) = 0;
+    //virtual void push_back(T value) = 0;
+    //virtual void push_front(T value) = 0;
+    virtual void pop() = 0;
+    //virtual void pop_front() = 0;
+    //virtual void pop_back() = 0;
     virtual void insert(int index, T value) = 0;
     virtual void remove(int index) = 0;
     virtual void find(string email) = 0;
     virtual void filter(int level) = 0;
+};
+
+template <class T>
+class SinglyLinkedList : public LinkedList<T> {
+private:
+    bool typeIsEqualToProgrammer() {
+        return typeid(T) == typeid(Programmer*);
+    }
+    int count;
+    Node<T>* head;
+public:
+    SinglyLinkedList() {
+        head = nullptr;
+        count = 0;
+    }
+    SinglyLinkedList(const T& data) {
+        count = 1;
+        head = new Node<T>(data);
+    }
+    ~SinglyLinkedList() {
+        while (count != 0) pop();
+    }
+    void push(T value) {
+        Node<T>* newNode = new Node<T>(value);
+        newNode->setNext(this->head);
+        this->head = newNode;
+        this->count++;
+    }
+    void pop() {
+        if (this->head) {
+            Node<T>* tempNode = this->head;
+            this->head = this->head->next;
+            this->count--;
+        }
+        else {
+            throw Exception("This is Empty\n");
+        }
+    }
+    void insert(int index, T value) {
+        if (index < 0 || index > this->count) {
+            throw Exception("Invalid index\n");
+        }
+        if (index == 0) {
+            push(value);
+            return;
+        }
+        Node<T>* newNode = new Node<T>(value);
+        Node<T>* prev = this->head;
+        for (int i = 0; i < index - 1; i++) {
+            prev = prev->getNext();
+        }
+        newNode->setNext(prev->getNext());
+        prev->setNext(newNode);
+        this->count++;
+    }
+    void remove(int index) {
+        if (index < 0 || index >= this->count) {
+            throw Exception("Invalid index\n");
+        }
+        if (index == 0) {
+            pop();
+            return;
+        }
+        Node<T>* prev = this->head;
+        for (int i = 0; i < index - 1; i++) {
+            prev = prev->getNext();
+        }
+        Node<T>* toDelete = prev->getNext();
+        prev->setNext(toDelete->getNext());
+        delete toDelete;
+        this->count--;
+    }
+    void find(string email) {
+        Node<T>* temporaryNode = this->head;
+        while (temporaryNode != NULL) {
+            T info = temporaryNode->getInfo();
+
+            if (typeid(T) == typeid(Programmer*)) {
+                Programmer* casted = dynamic_cast<Programmer*>(info);
+                if (casted->emailMatches(email)) {
+                    cout << casted->stringify() << endl;
+                    return;
+                }
+            }
+            temporaryNode = temporaryNode->next;
+        }
+        cout << "No matches were found. " << endl;
+    }
+    void filter(int level) {
+        Node<T>* temporaryNode = this->head;
+        while (temporaryNode != NULL) {
+            T info = temporaryNode->getInfo();
+            if (typeIsEqualToProgrammer()) {
+                Programmer* casted = dynamic_cast<Programmer*>(info);
+                if (casted->getLevel() == level) {
+                    cout << casted->stringify() << endl;
+                    return;
+                }
+            }
+            temporaryNode = temporaryNode->next;
+        }
+    }
+    void printList() {
+        Node<T>* temp = this->head;
+        int index = 0;
+        while (temp != NULL) {
+            if (typeIsEqualToProgrammer()) {
+                Programmer* casted = dynamic_cast<Programmer*>(temp->getInfo());
+                cout << to_string(index) + ": " + casted->stringify() << endl;
+            }
+            temp = temp->next;
+            index++;
+        }
+        if (temp == NULL) cout << "" << endl;
+    }
+    ofstream& save(ofstream& out) {
+        if (head == nullptr) {
+            cout << "List is empty\n";
+            return out;
+        }
+        if (out) {
+            out << count << '\n' << count << '\n';
+            Node<T>* current = head;
+
+            while (current != nullptr) {
+                if (current->info != nullptr) {
+                    out << *(current->info) << '\n';
+                }
+                current = current->next;
+            }
+            return out;
+        }
+    }
+    SinglyLinkedList<T>& load(ifstream& in) {
+        if (in) {
+            int temp_size, count;
+            in >> count >> temp_size;
+
+            for (int i = 0; i < temp_size; i++) {
+                T _data;
+                in >> _data;
+                push(_data);
+            }
+        }
+        return *this;
+    }
 };
 
 template<class T>
@@ -152,10 +300,10 @@ protected:
         return typeid(T) == typeid(Programmer*);
     }
 public:
-    template<class U>
-    friend istream& operator >> (istream& in, DoubleLinkedList<U>& list);
-    template<class U>
-    friend ostream& operator << (ostream& out, DoubleLinkedList<U>& list);
+    // template<class T>
+    friend istream& operator >> (istream& in, DoubleLinkedList<T>& list);
+    // template<class T>
+    friend ostream& operator << (ostream& out, DoubleLinkedList<T>& list);
     DoubleLinkedList() {
         head = tail = nullptr;
         count = 0;
@@ -165,16 +313,23 @@ public:
         head = tail = new Node<T>(data);
     }
     ~DoubleLinkedList() {
-        while (head != nullptr) {
-            Node<T>* temp = head;
-            head = head->next;
-            delete temp;
+        while (!(this->count == 0)) {
+            pop_front();
         }
     }
     int Count() { return count; }
     Node<T>* First() { return head; }
     Node<T>* Last() { return tail; }
     void setFirst(Node<T>* _head) { head = _head; }
+    // void push(T value) override {
+    //     push_back(value);
+    // }
+    // void pop() override {
+    //     pop_back();
+    // }
+    // void insert(int index, T value) override {
+    //     insert2(value, index);
+    // }
     void printList() {
         Node<T>* temp = this->head;
         int index = 0;
@@ -188,6 +343,22 @@ public:
         }
         if (temp == NULL) cout << "" << endl;
         delete temp;
+    }
+    void push(T value) {
+        Node<T>* newNode = new Node<T>(value);
+        newNode->setNext(this->head);
+        this->head = newNode;
+        this->count++;
+    }
+    void pop() {
+        if (this->head) {
+            Node<T>* tempNode = this->head;
+            this->head = this->head->next;
+            this->count--;
+        }
+        else {
+            throw Exception("This is Empty\n");
+        }
     }
     void push_back(T data) {
         Node<T>* node = new Node<T>(data);
@@ -213,7 +384,6 @@ public:
     void pop_front() {
         Node<T>* temp = head;
         head = head->next;
-        delete temp;
         count--;
     }
     void pop_back() {
@@ -227,7 +397,6 @@ public:
             prev->next = nullptr;
         if (temp == head)
             head = nullptr;
-        delete temp;
         count--;
         /*Node<T>* temp = head;
         Node<T>* prev = nullptr;
@@ -265,7 +434,7 @@ public:
         }
         count++;
     }
-    void remove(const int index) override {
+    void remove(const int index) {
         if (index<0 || index>count)
             throw Exception("Invalid Index");
 
@@ -330,12 +499,10 @@ public:
             while (current != nullptr) {
                 out << current->info << ' ';
                 current = current->next;
-                if (current != nullptr) {
-                    out << "\n " << *(current->info);
-                }
+                out << "\n " << *(current->info);
             }
+            return out;
         }
-        return out;
     }
     DoubleLinkedList<T>& load(ifstream& in) {
         if (in) {
@@ -373,15 +540,20 @@ ostream& operator << (ostream& out, DoubleLinkedList<T>& list) {
 }
 
 int main() {
-    DoubleLinkedList<Programmer*> programmers;
+ /*   DoubleLinkedList<Programmer*> programmers;*/
+    SinglyLinkedList<Programmer*> programmers;
     Programmer* p1 = new Programmer("Smith", "John", "johnsmith@gmail.com", "johnsmith", "johnsmith", "Python", "Google", 7);
     Programmer* p2 = new Programmer("Doe", "Jane", "janedoe@gmail.com", "janedoe", "janedoe", "C++", "Facebook", 8);
     Programmer* p3 = new Programmer("Builder", "Bob", "bob123@gmail.com", "bobthebuilder", "bobthebuilder", "Rust", "Netflix", 9);
 
-    programmers.push_back(p1);
-    programmers.push_back(p2);
-    programmers.push_back(p3);
-    programmers.printList();
+    programmers.push(p1);
+    programmers.push(p2);
+    programmers.push(p3);
+
+    //programmers.push_back(p1);
+    //programmers.push_back(p2);
+    //programmers.push_back(p3);
+    ///*programmers.printList();*/
 
     programmers.find("johnsmith@gmail.com");
     programmers.find("bob123@gmail.com");
@@ -395,15 +567,34 @@ int main() {
         fout.close();
     }
     
+    
+    
+    /*
+
+    DoubleLinkedList<Programmer*> programmers2;
+
+    ifstream fin("in.txt");
+    if (!fin) {
+        fin >> programmers2;
+        fin.close();
+        cout << programmers2 << "\n";
+    }*/
+    
+
     //programmers.find("johnsmith@gmail.com");
     //programmers.filter(7);
 
     //programmers.remove(1);
     ///*programmers.printList();*/
 
-    programmers.pop_back();
-    programmers.pop_back();
-    programmers.pop_back();
+    programmers.pop();
+    programmers.pop();
+    programmers.pop();
+
+    //programmers.pop_back();
+    //programmers.pop_back();
+    //programmers.pop_back();
+    //programmers.printList();
 
     delete p1;
     delete p2;
--- zadacha8.cpp
+++ zadacha8.cpp
@@ -16,15 +16,23 @@ public:
 	Exception(const char* s) {
 		str = new char[strlen(s) + 1];
 		if (str != NULL) {
-			strcpy_s(str, strlen(s) + 1, s);
+			strcpy(str, s);
 		}
 	}
 	Exception(const Exception& e) {
 		str = new char[strlen(e.str) + 1];
 		if (str != NULL) {
-			strcpy_s(str, strlen(e.str) + 1 , e.str);
+			strcpy(str, e.str);
 		}
 	}
+	Exception& operator=(const Exception& other) {
+        if (this != &other) {
+            delete[] str;
+            str = new char[strlen(other.str) + 1];
+            strcpy(str, other.str);
+        }
+        return *this;
+    }
 	~Exception() {
 		if (str != NULL) {
 			delete[] str;
@@ -41,10 +49,10 @@ public:
 	T key;
 	Node* left;
 	Node* right;
-	Node<T>(T key): key(key), left(nullptr), right(nullptr){}
+	Node<T>(T key): key(key), left(NULL), right(NULL){}
 	Node<T>() {
 		key = 0;
-		left = right = nullptr;
+		left = right = NULL;
 	}
 	template<class U>
 	friend ostream& operator << (ostream& out, const Node<U>& node);
@@ -284,7 +292,282 @@ istream& operator >> (istream& in, SplayTree<T>& st) {
 	return in;
 }
 
+template<class T>
+class AVLNode : public Node<T> {
+public:
+	int height;
+	T key;
+	AVLNode<T>* left;
+	AVLNode<T>* right;
+	AVLNode(T key): Node<T>(key), height(1) {}
+	AVLNode<T>() {
+		key = 0;
+		left = right = nullptr;
+		height = 0;
+	}
+	template<class U>
+	friend ostream& operator << (ostream& out, const AVLNode<U>& avlnode);
+	template<class U>
+	friend istream& operator >> (istream& in, AVLNode<U>& avlnode);
+};
+
+template<class T>
+ostream& operator << (ostream& out, const AVLNode<T>& avlnode) {
+	out << "Key: " << avlnode.key;
+	if (avlnode.left != nullptr)
+		out << " | Left child: " << avlnode.left->key;
+	if (avlnode.right != nullptr)
+		out << " | Right child: " << avlnode.right->key;
+	return out;
+}
+
+template<class T>
+istream& operator >> (istream& in, AVLNode<T>& avlnode) {
+	in >> avlnode.key;
+	return in;
+}
+
+template<class T>
+class AVLTree {
+protected:
+	void deleteTree(AVLNode<T>* avlnode) {
+		if (avlnode == nullptr) {
+			return;
+		}
+		deleteTree(avlnode->left);
+		deleteTree(avlnode->right);
+		delete avlnode;
+	}
+	void savetofilehelper(AVLNode<T>* avlnode, ofstream& file) {
+		if (avlnode) {
+			savetofilehelper(avlnode->left, file);
+			file << avlnode->key << endl;
+			savetofilehelper(avlnode->right, file);
+		}
+	}
+	template<class U>
+	void writeKeysInOrder(AVLNode<U>* avlnode, ostream& out) {
+		if (avlnode) {
+			writeKeysInOrder(avlnode->left, out);
+			out << avlnode->key;
+			if (avlnode->left) out << " | left child: " << avlnode->left->key;
+			if (avlnode->right) out << " | right child: " << avlnode->right->key;
+			out << endl;
+			writeKeysInOrder(avlnode->right, out);
+		}
+	}
+public:
+	AVLNode<T>* root;
+	AVLTree() : root(nullptr) {}
+	~AVLTree() {
+		deleteTree(root);
+	}
+	int height(AVLNode<T>* avlnode) {
+		if (avlnode == nullptr) return 0;
+		return avlnode->height;
+	}
+	int max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+	AVLNode<T>* newNode(T key) {
+		AVLNode<T>* node = new AVLNode<T>();
+		node->key = key;
+		node->left = NULL;
+		node->right = NULL;
+		node->height = 1;
+		return node;
+	}
+	AVLNode<T>* rightRotate(AVLNode<T>* y) {
+		if (y == nullptr) throw Exception("Cannot rotate a null node\n");
+		AVLNode<T>* x = y->left;
+		if (x == nullptr) throw Exception("Cannot rotate a node, because it has no left child\n");
+		AVLNode<T>* t2 = x->right;
+		x->right = y;
+		y->left = t2;
+		y->height = max(height(y->left), height(y->right) + 1);
+		x->height = max(height(x->left), height(x->right) + 1);
+		return x;
+	}
+	AVLNode<T>* leftRotate(AVLNode<T>* x) {
+		if (x == nullptr) {
+			throw Exception("Cannot rotate a null node");
+		}
+		AVLNode<T>* y = x->right;
+		if (y == nullptr) {
+			throw Exception("Cannot rotate a node, because it has no right child");
+		}
+		AVLNode<T>* t2 = y->left;
+		y->left = x;
+		x->right = t2;
+		x->height = max(height(x->left), height(x->right) + 1);
+		y->height = max(height(y->left), height(y->right) + 1);
+		return y;
+	}
+	int getBalance(AVLNode<T>* avlnode) {
+		if (avlnode == NULL) return 0;
+		return height(avlnode->left) - height(avlnode->right);
+	}
+	AVLNode<T>* search(T key, AVLNode<T>* root) {
+		if (root == nullptr || root->key == key)
+			return root;
+
+		if (root->key < key)
+			return search(key, root->right);
+
+		return search(key, root->left);
+	}
+	AVLNode<T>* insert(AVLNode<T>* avlnode, int key) {
+		if (avlnode == NULL) return(newNode(key));
+		if (key < avlnode->key) avlnode->left = insert(avlnode->left, key);
+		else if (key > avlnode->key) avlnode->right = insert(avlnode->right, key);
+		else return avlnode;
+		avlnode->height = 1 + max(height(avlnode->left), height(avlnode->right));
+		int balance = getBalance(avlnode);
+		if (balance > 1 && key > avlnode->left->key) {
+			avlnode->left = leftRotate(avlnode->left);
+			return rightRotate(avlnode);
+		}
+		if (balance < -1 && key < avlnode->right->key) {
+			avlnode->right = rightRotate(avlnode->right);
+			return leftRotate(avlnode);
+		}
+		return avlnode;
+	}
+	AVLNode<T>* minValueAVLN(AVLNode<T>* avlnode) {
+		AVLNode<T>* current = avlnode;
+		while (current->left != nullptr) current = current->left;
+		return current;
+	}
+	AVLNode<T>* deletenode(AVLNode<T>* root, int key) {
+		if (root == NULL) return root;
+		if (key < root->key) root->left = deletenode(root->left, key);
+		else if (key > root->key) root->right = deletenode(root->right, key);
+		else {
+			if ((root->left == NULL) || (root->right == NULL)) {
+				AVLNode<T>* temp = root->left ?
+								   root->left :
+								   root->right;
+				if (temp == NULL) {
+					temp = root;
+					root = NULL;
+				}
+				else *root = *temp;
+				delete temp;
+			}
+			else {
+				AVLNode<T>* temp = minValueAVLN(root->right);
+				root->key = temp->key;
+				root->right = deletenode(root->right, temp->key);
+			}
+		}
+
+		if (root == NULL) return root;
+
+		root->height = 1 + max(height(root->left), height(root->right));
+		int balance = getBalance(root);
+		if (balance > 1 && getBalance(root->left) >= 0) return rightRotate(root);
+		if (balance > 1 && getBalance(root->left) < 0) {
+			root->left = leftRotate(root->left);
+			return rightRotate(root);
+		}
+		if (balance < -1 && getBalance(root->right) > 0) {
+			root->right = rightRotate(root->right);
+			return leftRotate(root);
+		}
+		return root;
+	}
+	void preOrder(AVLNode<T>* root) {
+		if (root != NULL) {
+			cout << root->key << " ";
+			preOrder(root->left);
+			preOrder(root->right);
+		}
+	}
+	void save(const char* c) {
+		ofstream outfile(c);
+		if (!outfile.is_open()) {
+			throw Exception("Failed to open file");
+		}
+		savetofilehelper(root, outfile);
+		outfile.close();
+	}
+
+	AVLNode<T>* load(ifstream& in) {
+		if (!in.is_open()) {
+			throw Exception("Could not open file");
+		}
+		int key;
+		while (in >> key) {
+			root = insert(root, key);
+		}
+		return root;
+	}
+
+	template<class U>
+	friend ostream& operator << (ostream& out, AVLTree<U>& avltree);
+	template<class U>
+	friend istream& operator >> (istream& in, AVLTree<U>& avltree);
+};
+
+template<class T>
+ostream& operator << (ostream& out, AVLTree<T>& avltree) {
+	writeKeysInOrder(avltree.root, out);
+	return out;
+}
+
+template<class T>
+istream& operator >>(istream& in, AVLTree<T>& avltree) {
+	T key;
+	while (in >> key) {
+		avltree.root = avltree.insert(avltree.root, key);
+	}
+	return in;
+}
+
 int main() {
+	AVLTree<int> avl;
+	ifstream fin("test.txt");
+	if (fin) {
+		fin >> avl;
+		fin.close();
+	}
+	else {
+		cout << "Couldn't open the file. \n";
+		return 1;
+	}
+	//try {
+	//	avl.root = avl.rightRotate(avl.root);
+	//}
+	//catch (const Exception& e) {
+	//	cout << "Exception was caught: " << endl;
+	//	e.print();
+	//}
+	avl.preOrder(avl.root);
+	//try {
+	//	avl.load(fin);
+	//}
+	//catch (const Exception& e) {
+	//	e.print();
+	//}
+	ofstream fout("result.txt");
+	if (fout) {
+		fout << avl;
+		fout.close();
+	}
+
+	//avl.root = avl.insert(avl.root, 10);
+	//avl.root = avl.insert(avl.root, 20);
+	//avl.root = avl.insert(avl.root, 30);
+	//avl.root = avl.insert(avl.root, 40);
+	//avl.root = avl.insert(avl.root, 50);
+	//avl.root = avl.leftRotate(avl.root);
+	//AVLNode<int>* result = avl.search(20, avl.root);
+	//if (result != nullptr) cout << "Found\n";
+	//else cout << "Not Found\n";
+	//
+	//avl.root = avl.deletenode(avl.root, 30);
+	//avl.preOrder(avl.root);
+
 	//SplayTree<int> st;
 	//ifstream fin("test.txt");
 	//if (fin) {
